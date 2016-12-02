7ceb0617d62ea62aaafb4e9e4bd8ef75cc862e64
ripstick <wilsonjs0311@gmail.com>
2016-12-01 22:57:16 -0600

    user registration

--- project/recipes/forms.py
+++ project/recipes/forms.py
@@ -2,10 +2,10 @@
 
 
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, TextField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class AddRecipeForm(Form):
     recipe_title = StringField('Recipe Title', validators=[DataRequired()])
-    recipe_description = StringField('Recipe Description', validators=[DataRequired()])
+    recipe_description = TextAreaField('Recipe Description', validators=[DataRequired()])
